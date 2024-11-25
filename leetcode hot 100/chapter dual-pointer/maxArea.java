0589d512c9c07a86749fbcc886b08154450298a8
Hu shan <hushan@kmoon.fun>
2024-11-25 19:33:39 +0800

    Update maxArea.java

--- leetcode hot 100/chapter dual-pointer/maxArea.java
+++ leetcode hot 100/chapter dual-pointer/maxArea.java
@@ -1,11 +1,23 @@
 /*
  * LC.11. 盛最多水的容器
  * Level：medium
- * Tag：数组、双指针、贪心
+ * Tag：数组、双指针
  */
 
-class Solution {
+ class Solution {
     public int maxArea(int[] height) {
-        
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+         while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+         }
+         return ans;
     }
 }
